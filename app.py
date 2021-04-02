10e2f71ea42f1f1871385a7641949404488d05a0
White Ant <trafi227@gmail.com>
2021-04-02 14:36:20 +0600

    .

--- app.py
+++ app.py
@@ -3,8 +3,6 @@ from flask import Flask,request,render_template,url_for
 
 app = Flask(__name__)
 
-
-
 @app.route("/login")
 @app.route('/')
 def index():
@@ -13,5 +11,5 @@ def index():
 
 
 if __name__ == '__main__':
-     app.run()
-    
+    app.run()
+
