e110d4249d3b4a43400c989bd73d0e1262a80260
White Ant <trafi227@gmail.com>
2021-04-02 20:42:42 +0600

    .

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask import Flask,request,render_template,url_for
 
 app = Flask(__name__)
 
-@app.route("/login")
-@app.route("/signin")
+@app.route("/login/")
+@app.route("/signin/")
 @app.route('/')
 def index():
     return render_template("login.html")
 
-@app.route("/sinup")
+
+
+@app.route("/signup/")
 @app.route("/register")
 def signup():
-    render_template("/signup.html")
-
+    return render_template("/signup.html")
 
 
 
 
 if __name__ == '__main__':
-    app.run()
-
+    app.run(debug=True)
