1db8ce267b6e39130e08ff94a7beed56012b6f99
White Ant <trafi227@gmail.com>
2021-04-02 12:27:37 +0600

    .

--- app.py
+++ app.py
@@ -1,30 +1,18 @@
 from flask import Flask,request,render_template,url_for
 
 
-
 app = Flask(__name__)
+
+
+
+@app.route("/login")
 @app.route('/')
 def index():
-    return render_template("/login/login.html")
+    return render_template("login.html")
 
 
 
 
 if __name__ == '__main__':
      app.run()
-# def chechkdb():
-#     file = open("intls/DB.json")
-#     global db
-#     db = json.load(file)
-
-#     #writedatabase
-
-#json db writter
-
-# def writedb(user,passw,email,phone):
-#     chechkdb()
-#     x = {"name" : user,"email" :email,"num" : phone,"pass" : passw}
-#     temp  = db["clint"]
-#     temp.append(x)
-#     with open("intls/DB.json","w") as f:
-#        json.dump(db,f,indent=4)
+     
