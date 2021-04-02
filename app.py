53c7c0695478a5d8bbec9b64320c50672c1ec713
White Ant <trafi227@gmail.com>
2021-04-02 14:43:53 +0600

    .

--- app.py
+++ app.py
@@ -4,9 +4,16 @@ from flask import Flask,request,render_template,url_for
 app = Flask(__name__)
 
 @app.route("/login")
+@app.route("/signup")
 @app.route('/')
 def index():
     return render_template("login.html")
+
+@app.route("/sinup")
+@app.route("/register")
+def signup():
+    render_template("")
+
 
 
 
