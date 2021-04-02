18bf96f8bc0c3ec13a7142e9f0afa5a07d645482
White Ant <trafi227@gmail.com>
2021-04-03 01:28:34 +0600

    problem!
    kichu problem ase sokale thik kormu

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
-from flask import Flask,request,render_template,url_for
+from flask import Flask,request,render_template,url_for,session
 
 
 app = Flask(__name__)
+
 
 @app.route("/login/")
 @app.route("/signin/")
 @app.route('/')
 def index():
-    return render_template("login.html")
+    return render_template("signup.html")
 
 
 
-@app.route("/signup/")
-@app.route("/register")
-def signup():
-    return render_template("/signup.html")
+
+# @app.route("/signup/")
+# @app.route("/register")
+# def signup():
+#     return render_template("/signup.html")
 
 
-
+session.clear()
 
 if __name__ == '__main__':
     app.run(debug=True)
