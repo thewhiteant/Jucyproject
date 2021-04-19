33f4e0199bb3903fb798f2f13d583d47dfc7377b
White Ant <trafi227@gmail.com>
2021-04-20 02:31:12 +0600

    .

--- app.py
+++ app.py
@@ -1,21 +1,87 @@
-from flask import Flask,request,render_template,url_for,session
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
+#db connection
+
+# mydb = pymysql.connect(
+#     host="localhost",
+#     user="root",
+#     passwd="",
+#     db="test")
+
+# myc = mydb.cursor()
+# mydb.commit()
+
+
+
+
 
 
 app = Flask(__name__)
 
+app.secret_key = 'your secret key'
 
-@app.route("/login/")
-@app.route("/signin/")
-@app.route('/')
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'test'
+
+mysql = MySQL(app)
+
+
+
+
+@app.route('/',methods=['POST','GET'])
 def index():
-    return render_template("login.html")
+    msg = ""
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM jucy WHERE username = %s AND password = %s', (username, password,))
+        account = cursor.fetchone()
+        if account:
+            session['loggedin'] = True
+            session['id'] = account['id']
+            session['username'] = account['username']
+            return 'Logged in successfully!'
+        else:
+            msg = 'Incorrect username/password!'
+
+        
+
+    return render_template("login.html",msg=msg)
 
 
 
-@app.route("/signup/")
-@app.route("/register")
+@app.route("/signup",methods=['POST','GET'])
 def signup():
-    return render_template("/signup.html")
+
+    msg =""
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        usere = request.form['username']
+        passw = request.form['password']
+        age = request.form['age']
+        emil = request.form['email']
+        cursor1 = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        uposthiti = cursor1.execute('SELECT * FROM jucy WHERE username = %s', (usere,))
+        print(uposthiti)
+        if uposthiti == 1:
+                msg  = "Username alrady taken"
+                return render_template("/signup.html", msg=msg)
+
+        elif uposthiti == 0:
+            x = cursor1.execute("SELECT * FROM jucy WHERE id")
+            cursor1.execute(f"INSERT INTO jucy(id,username,password,age,email) VALUES({x+1},'{usere}','{passw}','{age}','{emil}')")
+            mysql.connection.commit()
+            return "Registration Done!"
+        else:
+            return "404 error"
+            
+
+    return render_template("/signup.html",msg=msg)
 
 
 @app.route("/reset")
@@ -23,4 +89,7 @@ def reset():
         return render_template("/reset.html")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+
+
+
